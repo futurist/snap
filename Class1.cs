4aaccad189fb19bb13e067ca2c1ec724c4cb427b
futurist <jamesyang999@gmail.com>
2017-02-01 17:02:46 +0800

    add: coord recorded

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace snap
 {
@@ -12,9 +14,12 @@ namespace snap
     {
 
         string capFolder = @"data\";
+        string newName;
+        StringBuilder strOut = new StringBuilder();
 
         public Class1()
         {
+
 
             if (!Directory.Exists(Path.GetDirectoryName(capFolder)))
             {
@@ -37,26 +42,38 @@ namespace snap
 
             index = index + 1;
 
-            string newName = index.ToString().PadLeft(3, '0') + ".png";
+            newName = index.ToString().PadLeft(3, '0');
             Console.WriteLine(newName);
 
-            startCap(newName);
+            startCap(newName + ".png");
 
         }
 
         void startCap(string name)
         {
-            runExe(capFolder + name, "boxcutter.exe", waitForSnap);
+            runExe(capFolder + name, "boxcutter.exe", waitForSnap, strOut);
         }
 
         void waitForSnap(object sender2, System.EventArgs e2)
         {
-            Console.WriteLine("box finished");
+            string str = strOut.ToString();
+
+            MatchCollection matches = Regex.Matches(str.Split('\n').First(), @"\d+");
+
+            if (!str.Contains("coords:") || matches.Count < 4) Application.Exit();
+
+            string result = "";
+            for (int i = 0; i < 4; i++)
+            {
+                result += matches[i].ToString() + ",";
+            }
+
+            File.WriteAllText(capFolder + newName + ".txt", result.TrimEnd(','));
+            Console.WriteLine("box finished:" + str);
             Application.Exit();
         }
 
-
-        Process runExe(string arg, string exePath, Action<object, EventArgs> onExit)
+        static Process runExe(string arg, string exePath, Action<object, EventArgs> onExit, StringBuilder outputBuilder)
         {
             string path = Directory.GetCurrentDirectory();
             DirectoryInfo d = new DirectoryInfo(path);
@@ -69,18 +86,38 @@ namespace snap
             startInfo.UseShellExecute = true;
             startInfo.FileName = exePath;
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.WorkingDirectory = d.FullName;
+            // startInfo.WorkingDirectory = d.FullName;
             startInfo.Arguments = arg;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.CreateNoWindow = true;
 
-            myProcess.StartInfo = startInfo;
+            if (outputBuilder != null)
+            {
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardError = true;
+                startInfo.RedirectStandardOutput = true;
+                myProcess.OutputDataReceived += (sender, eventArgs) => outputBuilder.AppendLine(eventArgs.Data);
+                myProcess.ErrorDataReceived += (sender, eventArgs) => outputBuilder.AppendLine(eventArgs.Data);
+            }
+
+
             if (onExit != null)
             {
                 myProcess.EnableRaisingEvents = true;
                 myProcess.Exited += new EventHandler(onExit);
             }
+
+            myProcess.StartInfo = startInfo;
             myProcess.Start();
+
+            if (outputBuilder != null)
+            {
+                myProcess.BeginOutputReadLine();
+                myProcess.BeginErrorReadLine();
+            }
 
             return myProcess;
         }
+
     }
 }
